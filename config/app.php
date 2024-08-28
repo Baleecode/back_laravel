3ada41624995133a35ed44f691f72924d784e5a4
mrbell21 <hostinginweb7@gmail.com>
2024-08-28 18:28:22 +0700

    Update app.php

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://back-laravel.vercel.app'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL'),
 
